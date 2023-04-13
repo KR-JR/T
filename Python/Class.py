6f2e27f00137320c44ef481ef905e0e33d0fda48
KR-JR <skflwh72@gmail.com>
2023-04-13 22:27:50 +0900

    Apr 4.13

--- Python/Class.py
+++ Python/Class.py
@@ -1,5 +1,11 @@
-import datetime
+score = [[50, 70, 50, 30], [20, 50, 70, 90]]
+sum = 0
+print(score)
+print(score[0][0], score[0][1], score[1][1], score[1][3])
 
-today = datetime.datetime.now()
-print(str(today))
-print(repr(today))
+
+for i in range(0, 2):
+    for j in range(0, 4):
+        sum += score[i][j]
+
+print(sum)
