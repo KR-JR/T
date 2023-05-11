1a1b8352ca6f3df96eaa5d3b85c3d59177011857
KR-JR <skflwh72@gmail.com>
2023-05-11 16:55:28 +0900

    05.11 last push

--- Python/Class.py
+++ Python/Class.py
@@ -1,27 +1,13 @@
-import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import metrics
+class Shark:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+new_shark = Shark("Sanny", 5)
+print(new_shark.name)
+print(new_shark.age)
 
-dach_length = [77,78,85,83,73,77,73,80]
-dach_height = [25,28,19,30,21,22,17,35]
 
-samo_length = [75,77,86,86,79,83,83,88]
-samo_height = [56,57,50,53,60,53,49,61]
-
-d_data = np.column_stack((dach_length, dach_height))
-d_label = np.zeros(len(d_data))
-s_data = np.column_stack((samo_length, samo_height))
-s_label = np.ones(len(s_data))
-
-newdata = [[79,35]]
-
-dogs = np.concatenate((d_data,s_data))
-labels = np.concatenate((d_label,s_label))
-
-dog_classes = {0:'Dachshund',1:'Samoyed'}
-
-k=3
-knn = KNeighborsClassifier(n_neighbors = k)
-knn.fit(dogs, labels)
-y_pred = knn.predict(newdata)
-print('데이터',newdata,',판정결과:',dog_classes[y_pred[0]])
+me = Shark("JaeHun", 24)
+print(me.name)
+print(me.age)
