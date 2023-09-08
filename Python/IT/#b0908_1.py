f0903976efa6f313a0161147f7fdad4e36ec9756
KR-JR <skflwh72@gmail.com>
2023-09-08 14:42:39 +0900

    09.08-1

---
+++ Python/IT/#b0908_1.py
@@ -0,0 +1,38 @@
+print("LEE")
+print('LEE')
+
+
+print('LEE \
+      JAEHUN')
+
+print("""LEE
+      JAEHUN""")
+
+print('''LEE
+      JAEHUN''')
+
+print('LEE', end = ' ')
+print('JAEHUN')
+
+print('Lee', end = '&&&')
+print('JAEHUN')
+
+print('LEE', 'JAEHUN', sep = '#')
+
+f = open('dump.txt', 'w')
+print('LEE JAEHUN', file = f)
+f.cloase()
+
+import sys
+print('LEE JAEHUN', file = sys.stderr)
+
+
+print('My mother\'shouse')
+
+print('\',\",\\,\a,\t,\n')
+
+i = 123
+f = 3.14
+s = 'LEE'
+
+print()
