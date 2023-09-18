423e845dd7b5ee3f97980a0b4f74a958e775aea7
KR-JR <skflwh72@gmail.com>
2023-09-18 16:04:23 +0900

    09.18-1 push

--- Private/Network/test.c
+++ Private/Network/test.c
@@ -1,16 +1,5 @@
 #include<stdio.h>
 
-double getPrice() {
-    return basePrice() * discountFactor();
+int main() {
+    printf("±î²á");
 }
-
-double discountFactor() {
-    if (basePrice() > 1000) discountFactor() = 0.95;
-    else discountFactor() = 0.98;
-    return;
-}
-
-int basePrice() {
-    return _quantity * _itemPrice;
-}
-
