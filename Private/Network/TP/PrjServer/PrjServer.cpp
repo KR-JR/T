b2347da60c6908bb3684273215baf5352c29230b
KR-JR <skflwh72@gmail.com>
2023-12-02 00:28:48 +0900

    12.02 first

--- Private/Network/TP/PrjServer/PrjServer.cpp
+++ Private/Network/TP/PrjServer/PrjServer.cpp
@@ -1,8 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include "Common.h"
+#include "..\..\Common.h"
 
 #define SERVERPORT 9000
-#define BUFSIZE    256
+#define BUFSIZE    512
 #define SIZE_TOT 256                    // 전송 패킷(헤더 + 데이터) 전체 크기
 #define SIZE_DAT (SIZE_TOT-sizeof(int)) // 헤더를 제외한 데이터 부분만의 크기
 #define TYPE_CHAT     1000              // 메시지 타입: 채팅
@@ -111,11 +111,11 @@ int main(int argc, char* argv[])
 	// 소켓 생성
 	SOCKET listen_sock4 = socket(AF_INET, SOCK_STREAM, 0);
 	if (listen_sock4 == INVALID_SOCKET) err_quit("socket()");
-
-	// 넌블로킹 소켓 옵션 추가
-	u_long on = 1;
-	ioctlsocket(listen_sock4, FIONBIO, &on);
-
+	{
+		// 넌블로킹 소켓 옵션 추가
+		u_long on = 1;
+		ioctlsocket(listen_sock4, FIONBIO, &on);
+	}
 	// bind()
 	struct sockaddr_in serveraddr;
 	memset(&serveraddr, 0, sizeof(serveraddr));
@@ -135,11 +135,11 @@ int main(int argc, char* argv[])
 	// 소켓 생성
 	SOCKET listen_sock6 = socket(AF_INET6, SOCK_STREAM, 0);
 	if (listen_sock6 == INVALID_SOCKET) err_quit("socket()");
-	
-	// 넌블로킹 소켓 옵션 추가
-	u_long on = 1;
-	ioctlsocket(listen_sock6, FIONBIO, &on);
-
+	{
+		// 넌블로킹 소켓 옵션 추가
+		u_long on = 1;
+		ioctlsocket(listen_sock6, FIONBIO, &on);
+	}
 	// bind()
 	struct sockaddr_in6 serveraddr6;
 	memset(&serveraddr6, 0, sizeof(serveraddr6));
@@ -211,12 +211,15 @@ int main(int argc, char* argv[])
 	struct sockaddr_in clientaddr4;
 	// 데이터 통신에 사용할 변수(IPv6)
 	struct sockaddr_in6 clientaddr6;
+	char buffer[1024];
 
 	while (1) {
 		// 소켓 셋 초기화
 		FD_ZERO(&rset);
 		FD_SET(listen_sock4, &rset);
 		FD_SET(listen_sock6, &rset);
+		FD_SET(udp_sock4, &rset);
+		FD_SET(udp_sock6, &rset);
 		for (int i = 0; i < nTotalSockets; i++) {
 			FD_SET(SocketInfoArray[i]->sock, &rset);
 		}
@@ -225,8 +228,7 @@ int main(int argc, char* argv[])
 		retval = select(0, &rset, NULL, NULL, NULL);
 		if (retval == SOCKET_ERROR) err_quit("select()");
 
-		// 소켓 셋 검사(1): 클라이언트 접속 수용 
-		// UDP/IPV4
+		// select() 후 UDP 소켓 이벤트 처리
 		if (FD_ISSET(udp_sock4, &rset)) {
 			// IPv4 UDP 소켓에서 데이터 수신
 			struct sockaddr_in src_addr;
@@ -240,21 +242,9 @@ int main(int argc, char* argv[])
 			}
 		}
 
-		// UDP/IPV6
-		if (FD_ISSET(udp_sock6, &rset)) {
-			// IPv4 UDP 소켓에서 데이터 수신
-			struct sockaddr_in src_addr;
-			int src_addr_len = sizeof(src_addr);
-			char udp_buf[BUFSIZE];
-			int recv_len = recvfrom(udp_sock6, udp_buf, BUFSIZE, 0, 
-									(struct sockaddr *)&src_addr, &src_addr_len);
-			if (recv_len > 0) {
-				// 수신된 데이터 처리
-				// 예: printf("Received UDP data: %s\n", udp_buf);
-			}
-		}		
-		
-		// TCP
+
+
+		// 소켓 셋 검사(1): 클라이언트 접속 수용
 		if (FD_ISSET(listen_sock4, &rset)) { // TCP/IPv4
 			addrlen = sizeof(clientaddr4);
 			client_sock = accept(listen_sock4,
@@ -275,7 +265,7 @@ int main(int argc, char* argv[])
 				}
 				else {
 					SendStoredMessages(client_sock);
-				}				
+				}
 			}
 		}
 		if (FD_ISSET(listen_sock6, &rset)) { // TCP/IPv6
@@ -298,158 +288,209 @@ int main(int argc, char* argv[])
 				}
 				else {
 					SendStoredMessages(client_sock);
-				}				
+				}
+
 			}
 		}
-
-		// 소켓 셋 검사(2): 데이터 통신
-		// 데이터 수신 부분의 수정
-		for (int i = 0; i < nTotalSockets; i++) {
-			SOCKETINFO* ptr = SocketInfoArray[i];
-			if (FD_ISSET(ptr->sock, &rset)) {
-
-
-				// 먼저 COMM_MSG의 헤더만큼만 받아 메시지 타입을 확인
-				retval = recv(ptr->sock, (char*)&(ptr->msg), sizeof(int), 0);
-				if (retval <= 0) {
-					RemoveSocketInfo(i);
-					continue;
-				}
-
-				// 메시지 타입에 따라 나머지 부분을 받음
-				switch (ptr->msg.type) {
-				case TYPE_CHAT: {
-					retval = recv(ptr->sock, (char*)&(ptr->msg) + sizeof(int), sizeof(CHAT_MSG) - sizeof(int), 0);
-					if (retval <= 0) {
-						RemoveSocketInfo(i);
-						continue;
-					}
-					StoreMessage((CHAT_MSG*)&(ptr->msg));
-					CHAT_MSG* chatMsg = (CHAT_MSG*)&(ptr->msg);
-					printf("[%s]: %s\n", chatMsg->clientID,chatMsg->msg);
-					// 다른 클라이언트에게 CHAT_MSG 전달
-					for (int j = 0; j < nTotalSockets; j++) {
-						if (i != j) { // 메시지를 보낸 클라이언트 제외
-							SOCKETINFO* ptr2 = SocketInfoArray[j];
-							send(ptr2->sock, (char*)chatMsg, sizeof(CHAT_MSG), 0);
-						}
-					}
-
-					// 자신에게도 메시지 전달
-					send(ptr->sock, (char*)chatMsg, SIZE_TOT, 0);
+			// UDP/IPv4 데이터 수신
+			if (FD_ISSET(udp_sock4, &rset)) {
+				addrlen = sizeof(clientaddr4);
+				retval = recvfrom(udp_sock4, buffer, sizeof(buffer), 0,
+					(struct sockaddr*)&clientaddr4, &addrlen);
+				if (retval == SOCKET_ERROR) {
+					err_display("recvfrom()");
 					break;
 				}
-				case TYPE_DRAWLINE: {
-					retval = recv(ptr->sock, (char*)&(ptr->msg) + sizeof(int), sizeof(DRAWLINE_MSG) - sizeof(int), 0);
-					if (retval <= 0) {
-						RemoveSocketInfo(i);
-						continue;
+				else {
+					// 클라이언트 정보 출력
+					char addr[INET_ADDRSTRLEN];
+					inet_ntop(AF_INET, &clientaddr4.sin_addr, addr, sizeof(addr));
+					printf("[UDP/IPv4 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+						addr, ntohs(clientaddr4.sin_port));
+					// 데이터 처리: 문자열 출력
+					buffer[retval] = '\0';  // NULL 문자 추가
+					printf("받은 메시지: %s\n", buffer);
+					if (!AddSocketInfo(client_sock, true, false)) {
+						closesocket(client_sock);
 					}
-					DRAWLINE_MSG* drawLineMsg = (DRAWLINE_MSG*)&(ptr->msg);
-					// DRAWLINE_MSG 처리 로직 (예: 로깅, 브로드캐스트 등)
-					printf("[DRAWLINE 메시지] 시작점: (%d, %d), 끝점: (%d, %d), 색상: %d\n",
-						drawLineMsg->x0, drawLineMsg->y0, drawLineMsg->x1, drawLineMsg->y1, drawLineMsg->color);
-					// 다른 클라이언트에게 DRAWLINE_MSG 전달
-					for (int j = 0; j < nTotalSockets; j++) {
-						if (i != j) { // 메시지를 보낸 클라이언트 제외
-							SOCKETINFO* ptr2 = SocketInfoArray[j];
-							send(ptr2->sock, (char*)drawLineMsg, sizeof(DRAWLINE_MSG), 0);
-						}
+					else {
+						SendStoredMessages(client_sock);
 					}
-
-					// 자신에게도 메시지 전달
-					send(ptr->sock, (char*)drawLineMsg, SIZE_TOT, 0);
-					break;
-				}
-				case TYPE_ERASEPIC: {
-					retval = recv(ptr->sock, (char*)&(ptr->msg) + sizeof(int), sizeof(ERASEPIC_MSG) - sizeof(int), 0);
-					if (retval <= 0) {
-						RemoveSocketInfo(i);
-						continue;
-					}
-					ERASEPIC_MSG* erasePicMsg = (ERASEPIC_MSG*)&(ptr->msg);
-					// ERASEPIC_MSG 처리 로직 (예: 로깅, 브로드캐스트 등)
-					printf("[ERASEPIC 메시지] 그림 지우기 요청\n");
-					// 다른 클라이언트에게 ERASEPIC_MSG 전달
-					for (int j = 0; j < nTotalSockets; j++) {
-						if (i != j) { // 메시지를 보낸 클라이언트 제외
-							SOCKETINFO* ptr2 = SocketInfoArray[j];
-							send(ptr2->sock, (char*)erasePicMsg, sizeof(ERASEPIC_MSG), 0);
-						}
-					}
-
-					// 자신에게도 메시지 전달
-					send(ptr->sock, (char*)erasePicMsg, SIZE_TOT, 0);
-					break;
-				}
-				case TYPE_FILE: {
-					retval = recv(ptr->sock, (char*)&(ptr->msg4) + sizeof(int), sizeof(FILEDATA_MSG) - sizeof(int), 0);
-					if (retval <= 0) {
-						RemoveSocketInfo(i);
-						continue;
-					}
-					printf("[파일 전송 메시지] 파일 전송 요청\n");
-					printf("[전송된 파일] %s\n", ptr->msg4.filename);
-
-					char buf[BUFSIZE] = { 0, };
-					size_t bytesRead;
-
-					// 파일을 쓰기 모드로 열기 (쓰기 가능한 경로를 사용하세요)
-					FILE* fd = fopen("RecievedFile.txt", "wb");
-					if (fd == NULL) {
-						perror("파일 열기 실패");
-						exit(EXIT_FAILURE);
-					}
-
-					// 데이터를 수신하고 파일에 쓰기
-					while ((bytesRead = recv(ptr->sock, buf, sizeof(buf), 0)) > 0) {
-						size_t bytesWritten = fwrite(buf, sizeof(char), sizeof(buf), fd);
-						if (bytesWritten != bytesRead)	break;
-					}
-
-					// 파일과 소켓을 닫기
-					fclose(fd);
-
-					printf("파일 저장 완료\n");
-
-					break;
-				}
-				case TYPE_SHAPE:
-					retval = recv(ptr->sock, (char*)&(ptr->msg) + sizeof(int), sizeof(SHAPE_MSG) - sizeof(int), 0);
-					if (retval <= 0) {
-						RemoveSocketInfo(i);
-						continue;
-					}
-					SHAPE_MSG* drawShapeMsg = (SHAPE_MSG*)&(ptr->msg);
-					// DRAWSHAPE_MSG 처리 로직 (예: 로깅, 브로드캐스트 등)
-					printf("[DRAWSHAPE 메시지] 시작점: (%d, %d), 끝점: (%d, %d), 색상: %d\n",
-						drawShapeMsg->x0, drawShapeMsg->y0, drawShapeMsg->x1, drawShapeMsg->y1, drawShapeMsg->color);
-					// 다른 클라이언트에게 DRAWLINE_MSG 전달
-					for (int j = 0; j < nTotalSockets; j++) {
-						if (i != j) { // 메시지를 보낸 클라이언트 제외
-							SOCKETINFO* ptr2 = SocketInfoArray[j];
-							send(ptr2->sock, (char*)drawShapeMsg, sizeof(SHAPE_MSG), 0);
-						}
-					}
-
-					// 자신에게도 메시지 전달
-					send(ptr->sock, (char*)drawShapeMsg, SIZE_TOT, 0);
-					break;
 				}
 			}
-		} /* end of for */
+			// UDP/IPv6 데이터 수신
+			if (FD_ISSET(udp_sock6, &rset)) {
+				addrlen = sizeof(clientaddr6);
+				retval = recvfrom(udp_sock6, buffer, sizeof(buffer), 0,
+					(struct sockaddr*)&clientaddr6, &addrlen);
+				if (retval == SOCKET_ERROR) {
+					err_display("recvfrom()");
+					break;
+				}
+				else {
+					// 클라이언트 정보 출력
+					char addr[INET6_ADDRSTRLEN];
+					inet_ntop(AF_INET6, &clientaddr6.sin6_addr, addr, sizeof(addr));
+					printf("[UDP/IPv6 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+						addr, ntohs(clientaddr6.sin6_port));
+					// 데이터 처리: 문자열 출력
+					buffer[retval] = '\0';  // NULL 문자 추가
+					printf("받은 메시지: %s\n", buffer);
+					if (!AddSocketInfo(client_sock, true, false)) {
+						closesocket(client_sock);
+					}
+					else {
+						SendStoredMessages(client_sock);
+					}
+				}
+			}
 
-	} /* end of while (1) */
+			// 소켓 셋 검사(2): 데이터 통신
+			// 데이터 수신 부분의 수정
+			for (int i = 0; i < nTotalSockets; i++) {
+				SOCKETINFO* ptr = SocketInfoArray[i];
+				if (FD_ISSET(ptr->sock, &rset)) {
 
-	// 소켓 닫기
-	closesocket(listen_sock4);
 
-	// 윈속 종료
-	WSACleanup();
+					// 먼저 COMM_MSG의 헤더만큼만 받아 메시지 타입을 확인
+					retval = recv(ptr->sock, (char*)&(ptr->msg), sizeof(int), 0);
+					if (retval <= 0) {
+						RemoveSocketInfo(i);
+						continue;
+					}
 
-	void FreeMessageList();
+					// 메시지 타입에 따라 나머지 부분을 받음
+					switch (ptr->msg.type) {
+					case TYPE_CHAT: {
+						retval = recv(ptr->sock, (char*)&(ptr->msg) + sizeof(int), sizeof(CHAT_MSG) - sizeof(int), 0);
+						if (retval <= 0) {
+							RemoveSocketInfo(i);
+							continue;
+						}
+						StoreMessage((CHAT_MSG*)&(ptr->msg));
+						CHAT_MSG* chatMsg = (CHAT_MSG*)&(ptr->msg);
+						printf("[%s]: %s\n", chatMsg->clientID, chatMsg->msg);
+						// 다른 클라이언트에게 CHAT_MSG 전달
+						for (int j = 0; j < nTotalSockets; j++) {
+							if (i != j) { // 메시지를 보낸 클라이언트 제외
+								SOCKETINFO* ptr2 = SocketInfoArray[j];
+								send(ptr2->sock, (char*)chatMsg, sizeof(CHAT_MSG), 0);
+							}
+						}
 
-	return 0;
+						// 자신에게도 메시지 전달
+						send(ptr->sock, (char*)chatMsg, SIZE_TOT, 0);
+						break;
+					}
+					case TYPE_DRAWLINE: {
+						retval = recv(ptr->sock, (char*)&(ptr->msg) + sizeof(int), sizeof(DRAWLINE_MSG) - sizeof(int), 0);
+						if (retval <= 0) {
+							RemoveSocketInfo(i);
+							continue;
+						}
+						DRAWLINE_MSG* drawLineMsg = (DRAWLINE_MSG*)&(ptr->msg);
+						// DRAWLINE_MSG 처리 로직 (예: 로깅, 브로드캐스트 등)
+						printf("[DRAWLINE 메시지] 시작점: (%d, %d), 끝점: (%d, %d), 색상: %d\n",
+							drawLineMsg->x0, drawLineMsg->y0, drawLineMsg->x1, drawLineMsg->y1, drawLineMsg->color);
+						// 다른 클라이언트에게 DRAWLINE_MSG 전달
+						for (int j = 0; j < nTotalSockets; j++) {
+							if (i != j) { // 메시지를 보낸 클라이언트 제외
+								SOCKETINFO* ptr2 = SocketInfoArray[j];
+								send(ptr2->sock, (char*)drawLineMsg, sizeof(DRAWLINE_MSG), 0);
+							}
+						}
+
+						// 자신에게도 메시지 전달
+						send(ptr->sock, (char*)drawLineMsg, SIZE_TOT, 0);
+						break;
+					}
+					case TYPE_ERASEPIC: {
+						retval = recv(ptr->sock, (char*)&(ptr->msg) + sizeof(int), sizeof(ERASEPIC_MSG) - sizeof(int), 0);
+						if (retval <= 0) {
+							RemoveSocketInfo(i);
+							continue;
+						}
+						ERASEPIC_MSG* erasePicMsg = (ERASEPIC_MSG*)&(ptr->msg);
+						// ERASEPIC_MSG 처리 로직 (예: 로깅, 브로드캐스트 등)
+						printf("[ERASEPIC 메시지] 그림 지우기 요청\n");
+						// 다른 클라이언트에게 ERASEPIC_MSG 전달
+						for (int j = 0; j < nTotalSockets; j++) {
+							if (i != j) { // 메시지를 보낸 클라이언트 제외
+								SOCKETINFO* ptr2 = SocketInfoArray[j];
+								send(ptr2->sock, (char*)erasePicMsg, sizeof(ERASEPIC_MSG), 0);
+							}
+						}
+
+						// 자신에게도 메시지 전달
+						send(ptr->sock, (char*)erasePicMsg, SIZE_TOT, 0);
+						break;
+					}
+					case TYPE_FILE: {
+						retval = recv(ptr->sock, (char*)&(ptr->msg4) + sizeof(int), sizeof(FILEDATA_MSG) - sizeof(int), 0);
+						if (retval <= 0) {
+							RemoveSocketInfo(i);
+							continue;
+						}
+						printf("[파일 전송 메시지] 파일 전송 요청\n");
+						printf("[전송된 파일] %s\n", ptr->msg4.filename);
+
+						char buf[BUFSIZE] = { 0, };
+						size_t bytesRead;
+
+						// 파일을 쓰기 모드로 열기 (쓰기 가능한 경로를 사용하세요)
+						FILE* fd = fopen("RecievedFile.txt", "wb");
+						if (fd == NULL) {
+							perror("파일 열기 실패");
+							exit(EXIT_FAILURE);
+						}
+
+						// 데이터를 수신하고 파일에 쓰기
+						while ((bytesRead = recv(ptr->sock, buf, sizeof(buf), 0)) > 0) {
+							size_t bytesWritten = fwrite(buf, sizeof(char), sizeof(buf), fd);
+							if (bytesWritten != bytesRead)	break;
+						}
+
+						// 파일과 소켓을 닫기
+						fclose(fd);
+
+						printf("파일 저장 완료\n");
+
+						break;
+					}
+					case TYPE_SHAPE:
+						retval = recv(ptr->sock, (char*)&(ptr->msg) + sizeof(int), sizeof(SHAPE_MSG) - sizeof(int), 0);
+						if (retval <= 0) {
+							RemoveSocketInfo(i);
+							continue;
+						}
+						SHAPE_MSG* drawShapeMsg = (SHAPE_MSG*)&(ptr->msg);
+						// DRAWSHAPE_MSG 처리 로직 (예: 로깅, 브로드캐스트 등)
+						printf("[DRAWSHAPE 메시지] 시작점: (%d, %d), 끝점: (%d, %d), 색상: %d\n",
+							drawShapeMsg->x0, drawShapeMsg->y0, drawShapeMsg->x1, drawShapeMsg->y1, drawShapeMsg->color);
+						// 다른 클라이언트에게 DRAWLINE_MSG 전달
+						for (int j = 0; j < nTotalSockets; j++) {
+							if (i != j) { // 메시지를 보낸 클라이언트 제외
+								SOCKETINFO* ptr2 = SocketInfoArray[j];
+								send(ptr2->sock, (char*)drawShapeMsg, sizeof(SHAPE_MSG), 0);
+							}
+						}
+
+						// 자신에게도 메시지 전달
+						send(ptr->sock, (char*)drawShapeMsg, SIZE_TOT, 0);
+						break;
+					}
+				}
+			} /* end of for */
+
+		} /* end of while (1) */
+
+		// 소켓 닫기
+		closesocket(listen_sock4);
+
+		// 윈속 종료
+		WSACleanup();
+		return 0;
+	
 }
 
 
@@ -503,6 +544,7 @@ void RemoveSocketInfo(int nIndex)
 		printf("[TCP/IPv6 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n",
 			addr, ntohs(clientaddr.sin6_port));
 	}
+
 	// 소켓 닫기
 	closesocket(ptr->sock);
 	delete ptr;
@@ -511,7 +553,6 @@ void RemoveSocketInfo(int nIndex)
 		SocketInfoArray[nIndex] = SocketInfoArray[nTotalSockets - 1];
 	--nTotalSockets;
 }
-
 
 void StoreMessage(const CHAT_MSG* msg) {
 	MessageNode* newNode = (MessageNode*)malloc(sizeof(MessageNode));
