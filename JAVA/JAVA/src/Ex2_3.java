80793039535597e46e0e593bb96f2273b5a816ab
U-BOOK-D0OCQLF6VP\skflw <skflwh72@gmail.com>
2024-09-04 17:48:52 +0900

    09.04 push

---
+++ JAVA/JAVA/src/Ex2_3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ex2_3 {
+    public static void main(String[] args) {
+        final double rate = 1310.0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("원화를 입력하세요(단위 원)>>");
+        int won = scanner.nextInt();
+        double dollor = won/rate;
+
+        System.out.print(a);
+        System.out.print("원은");
+    }
+}
---
+++ JAVA/JAVA/src/Step1.java
@@ -0,0 +1,19 @@
+public class Step1 {
+    public static void main(String[] args) throws Exception {
+        
+        // System.out.println("홍길동\n\t홍\n\t\t길\n\t\t\t동");
+
+        // int a = 3;
+        // float b = 10.123f;
+        // System.out.println(a + b);
+        // System.out.println(a - b);
+        // System.out.println(a * b);
+        // System.out.println(a / b);
+
+        
+    
+
+    }
+}
+
+
