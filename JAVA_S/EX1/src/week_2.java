31c0ea5c5ab53680fe22df615e695878036f8a5d
U-BOOK-D0OCQLF6VP\skflw <skflwh72@gmail.com>
2024-09-10 16:08:35 +0900

    09.10

---
+++ JAVA_S/EX1/src/week_2.java
@@ -0,0 +1,11 @@
+public class week_2 {
+    public static void main(String[] args) {
+    // int i;
+    // i = 10;
+    // System.out.print(i);
+    
+        // double pi = 3.14;
+        // int pi2 = pi;
+        // System.out.print(pi2);    
+    }
+}
